638edc23d6bcb4249f43add66e1da0a9629913ef
Treysouz <treysouz@gmail.com>
2025-09-15 18:10:00 -0400

    [TSPS-12] - Added Card component.  Added tsParticles animated background

---
+++ src/lib/components/card/card.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children = undefined, class: className = '' } = $props();
+
+	//Add Inline Documentation
+</script>
+
+<div class="h-min w-min rounded-lg bg-black/50 p-4 shadow-md {className}">
+	{@render children?.()}
+</div>
