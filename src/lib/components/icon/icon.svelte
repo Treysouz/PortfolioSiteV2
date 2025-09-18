ba3cb61b5a03be5e58e81006c47a7fbbf6ab6d6b
Treysouz <treysouz@gmail.com>
2025-09-18 18:14:13 -0400

    [TSPS-9] - Fixed linting error

--- src/lib/components/icon/icon.svelte
+++ src/lib/components/icon/icon.svelte
@@ -14,5 +14,7 @@
 </script>
 
 <div class="inline-block {className}">
+	<!-- Safe to ignore this error since we are not injecting dynamic, user-inputter content-->
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	{@html svgs[svg]}
 </div>
