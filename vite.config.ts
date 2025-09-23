e717288622f9e510195511ee0d4bd6788e650b26
Treysouz <treysouz@gmail.com>
2025-09-23 11:09:44 -0400

    ci: [TSPS-21] - removed unncessary vite configuration that was causing workflow to fail

--- vite.config.ts
+++ vite.config.ts
@@ -30,36 +30,8 @@ export default defineConfig({
 				'**/playwright.config.*'
 			]
 		},
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'client',
-					environment: 'browser',
-					browser: {
-						enabled: true,
-						provider: 'playwright',
-						instances: [{ browser: 'chromium' }],
-						headless: true,
-						screenshotFailures: false
-					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				},
-				resolve: {
-					conditions: ['browser']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		exclude: ['src/lib/server/**']
 	}
 });
