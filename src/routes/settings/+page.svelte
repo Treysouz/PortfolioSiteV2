2c33c2315246de37016ffeb6e4c7a53f77ff96c4
Treysouz <treysouz@gmail.com>
2025-09-26 12:45:59 -0400

    fix: [TSPS-22] - Resolved accessibility error for settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,7 +1,7 @@
 <section id="about" class="flex min-h-screen w-full grow items-center px-8 py-8 sm:px-12">
 	<div class="flex h-full w-full flex-col space-y-8">
 		<div class="flex max-w-xl flex-row items-center space-x-4">
-			<h2 class="whitespace-nowrap text-3xl font-bold text-white">About Me</h2>
+			<h1 class="whitespace-nowrap text-3xl font-bold text-white">Settings</h1>
 			<div class="h-0.5 w-full bg-white"></div>
 		</div>
 	</div>
