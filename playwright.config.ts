8289361a86fb0bfc5515a24cf7aa0ea638180dc4
Treysouz <treysouz@gmail.com>
2025-09-29 10:54:00 -0400

    fix: [TSPS-30] - Returned playwright.config back to normal

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
-// Whether running E2E tests in CI
-const isCI = process.env.CI === 'true';
-
-// Build command to run based on whether isCI is true
-const buildCommand = `${isCI ? `SUPABASE_URL="${process.env.SUPABASE_URL}" SUPABASE_KEY="${process.env.SUPABASE_KEY}"` : ``} npm run build`;
-
 export default defineConfig({
 	webServer: {
-		command: `${buildCommand} && npm run preview`,
+		command: 'npm run build && npm run preview',
 		port: 4173
 	},
 	testDir: 'e2e'
