5fd943be2f620cc68d11194f41bb769935f3bfc9
Treysouz <treysouz@gmail.com>
2025-09-29 09:45:42 -0400

    fix: [TSPS-30] - Potential fix for E2E step in workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		env: {
+			SUPABASE_URL: process.env.SUPABASE_URL || '',
+			SUPABASE_KEY: process.env.SUPABASE_KEY || ''
+		}
 	},
 	testDir: 'e2e'
 });
