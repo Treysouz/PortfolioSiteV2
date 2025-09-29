e09eb589d83e3b566577710a3ac7f8bf606e0650
Treysouz <treysouz@gmail.com>
2025-09-29 10:05:24 -0400

    fix: [TSPS-30] - Potential fix for E2E step in workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from '@playwright/test';
 
+// Whether running E2E tests in CI
+const isCI = process.env.CI === 'true';
+
+// Build command to run based on whether isCI is true
+const buildCommand = `${isCI ? `SUPABASE_URL="${process.env.SUPABASE_URL}" SUPABASE_KEY="${process.env.SUPABASE_KEY}"` : ``} npm run build`;
+
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173,
-		env: {
-			SUPABASE_URL: process.env.SUPABASE_URL || '',
-			SUPABASE_KEY: process.env.SUPABASE_KEY || ''
-		}
+		command: `${buildCommand} && npm run preview`,
+		port: 4173
 	},
 	testDir: 'e2e'
 });
