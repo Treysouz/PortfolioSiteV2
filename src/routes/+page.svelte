5ce543ddd32cfc93e6e59e80987a7234b06fbb9e
Treysouz <treysouz@gmail.com>
2025-10-08 17:49:36 -0400

    feat: [TSPS-31] - Added footer component.  Switched to Cloudflare from Vercel.  Updated README

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Card from '$lib/components/card/card.svelte';
 	import { WelcomeSection, AboutSection, TechStackSection, ProjectSection } from '$lib/sections';
 </script>
 
@@ -12,4 +13,14 @@
 
 <ProjectSection></ProjectSection>
 
-<footer><!--Credits. Built by...icons from..etc.--></footer>
+<footer>
+	<div class="flex w-full flex-row items-center justify-center">
+		<Card class="w-full rounded-none p-4 text-center lg:w-auto lg:rounded-b-none lg:rounded-t-lg"
+			><span class="text-xs sm:text-sm"
+				>Copyright © 2025 Tremayne Souza — Front-End Developer
+			</span>
+			<span class="text-xs sm:text-sm">
+				All other copyrighted materials are property of their respective copyright holders
+			</span></Card>
+	</div>
+</footer>
